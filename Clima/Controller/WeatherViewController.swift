5a6ba2d5aa302ad17d7957d9553fbf3c970d4527
tylernevell <tylernevell@gmail.com>
2020-05-29 15:43:13 -0700

    Added searchTextField functions.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,49 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // tap into the delegate to set it as the current class; accessed through self property
+        // tells textfield to report back keyboard actions to view controller
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    // asks the delegate if the textfield should process the pressing of the return button
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Please type something."
+            return false
+        }
+    }
+    
+    // triggered whenever searchTextField.endEditing is called
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        // Use searchTextField.text to find weather for city
+        
+        searchTextField.text = ""
+    }
+    
 }
 
