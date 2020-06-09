2d576a725651a67c1a73db8c0a562bdc48b5b96c
tylernevell <tylernevell@gmail.com>
2020-06-08 18:39:50 -0700

    added WeatherData to handle JSON data. Added WeatherModel to choose weather graphic. Organized WeatherViewController with extensions to improve readability. Added methods to handle request for weather at User's current location.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -2,13 +2,11 @@
 //  ViewController.swift
 //  Clima
 //
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
 
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController, UITextFieldDelegate {
+class WeatherViewController: UIViewController {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -16,19 +14,35 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var searchTextField: UITextField!
     
     var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self
+
+        locationManager.requestWhenInUseAuthorization()
+        // for constant monitor and reporting of location, use startUpdatingLocatio() property
+        locationManager.requestLocation()
+        
         // tap into the delegate to set it as the current class; accessed through self property
         // tells textfield to report back keyboard actions to view controller
         searchTextField.delegate = self
+        weatherManager.delegate = self
     }
+    
+}
 
+//MARK: - UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+    
     @IBAction func searchPressed(_ sender: UIButton) {
         searchTextField.endEditing(true)
         print(searchTextField.text!)
     }
+    
     
     // asks the delegate if the textfield should process the pressing of the return button
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -49,6 +63,7 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    
     // triggered whenever searchTextField.endEditing is called
     func textFieldDidEndEditing(_ textField: UITextField) {
         
@@ -56,10 +71,50 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
         // if user enters valid string, pass the user input along to the WeatherManager class through cityName parameter
         if let city = searchTextField.text {
             weatherManager.fetchWeather(cityName: city)
+            cityLabel.text = city
         }
         
         // clear out input box
         searchTextField.text = ""
+    }
+}
+
+//MARK: - WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
+    
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+//MARK: - CLLocationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    
+    @IBAction func currentLocationButtonPressed(_ sender: UIButton) {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+        locationManager.stopUpdatingLocation()
+    }
+    
+    // failure to implement didFailWithError will result in a crash and is a programmer error
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
     }
     
 }
--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -7,22 +7,88 @@
 
 import Foundation
 
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
 struct WeatherManager {
     
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=0c4cb6fcdc98765edff5400edc5dec42&units=imperial"
+    var delegate: WeatherManagerDelegate?
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=0c4cb6fcdc98765edff5400edc5dec42&units=imperial"
     
-    
+    // for user search
     func fetchWeather(cityName: String) {
         
         let urlString = "\(weatherURL)&q=\(cityName)"
-        performRequest(urlString: urlString)
+        performRequest(with: urlString)
         
     }
     
-    func performRequest(urlString: String) {
-        //1. Create a URL
-        //2. Create a URLSession
-        //3. Give the Session a task
-        //4. Start the task
+    //for current location
+    func fetchWeather(latitude: Double, longitude: Double) {
+        
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
+        performRequest(with: urlString)
+        
     }
+    
+    func performRequest(with urlString: String) {
+        //1. Create a URL
+        if let url = URL(string: urlString) {
+            
+            //2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the Session a task
+            // let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            // hit enter to turn completionHandler into a closure automatically
+            let task = session.dataTask(with: url) { (data, response, error) in
+                // check for any errors
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                //
+                if let safeData = data {
+                    
+                    if let weather = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                    
+                    // value of the data object (JSON) doesn't directly translate into a string, so we need this string function to translate it
+                    //let dataString = String(data: safeData, encoding: .utf8)
+                    //print(dataString)
+                }
+            }
+            
+            //4. Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ weatherData: Data) -> WeatherModel? {
+        
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            return weather
+            
+            //print(weather.temperatureString)
+            
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+    
 }
